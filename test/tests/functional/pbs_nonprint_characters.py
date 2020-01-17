e71f408dde908abd0913d0a770e3abeecc76e14e
RKORavi <ravi.1si12is042@gmail.com>
2020-01-17 14:39:53 +0530

    test log

--- test/tests/functional/pbs_nonprint_characters.py
+++ test/tests/functional/pbs_nonprint_characters.py
@@ -149,8 +149,9 @@ sleep 5
                 j_output = '\n'.join(ret['out'])
             else:
                 j_output = ret['out'][0].strip()
-        print("JOUT ---------------------------- %s" % j_output)
-        print("CHECK VAR ------------------------ %s" % chk_var)
+        self.logger.info("RETURN VALUE ---------------------- %s" % ret['out'])
+        self.logger.info("JOUT ---------------------------- %s" % j_output)
+        self.logger.info("CHECK VAR ------------------------ %s" % chk_var)
         self.assertEqual(j_output, chk_var)
         self.logger.info('job output has: %s' % chk_var)
 
